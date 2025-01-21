c49afbe38ce7e8abe901830c37d03f39b9b6921a
Vindidogger <ecallery8@gmail.com>
2025-01-21 11:56:23 -0700

    Set 6.0 to SDA and 6.2 to SCL and verified they work as outputs

--- app/i2c.asm
+++ app/i2c.asm
@@ -3,36 +3,32 @@
             .cdecls C,LIST,"msp430.h"  ; Include device header file
 ;-------------------------------------------------------------------------------
 
-            .def    RESET                   ; Export program entry-point to
-                                            ; make it known to linker.
 
-            .global __STACK_END
-            .sect   .stack                  ; Make stack linker segment ?known?
+;------------------------------------------------------------------------------
+;           Constants
+;------------------------------------------------------------------------------
+SDA_PIN .equ BIT0
+SCL_PIN .equ BIT2
+SDA_OUT .equ P6OUT
+SCL_OUT .equ P6OUT
+SDA_DIR .equ P6DIR
+SCL_DIR .equ P6DIR            
 
-            .text                           ; Assemble to Flash memory
-            .retain                         ; Ensure current section gets linked
-            .retainrefs
 
-RESET       mov.w   #__STACK_END,SP         ; Initialize stack pointer
-
-
-init:
+init_i2c:
             ; stop watchdog timer
             mov.w   #WDTPW+WDTHOLD,&WDTCTL
+
+            ; Setup SCL and SDA
+            mov.b   #00h, &P1SEL0           ; sets to digital IO
+            mov.b   #00h, &P1SEL1
+
+            bis.b   #SDA_PIN, SDA_DIR       ; set SDA and SCL as output
+            bis.b   #SCL_PIN, SCL_DIR 
+            bis.b   #SDA_PIN, SDA_OUT       ; set SDA and SCL to High
+            bis.b   #SCL_PIN, SCL_OUT
+
 
             ; Disable low-power mode
             bic.w   #LOCKLPM5,&PM5CTL0
 
-main:
-
-            nop 
-            jmp main
-            nop
-
-
-
-;------------------------------------------------------------------------------
-;           Interrupt Vectors
-;------------------------------------------------------------------------------
-            .sect   RESET_VECTOR            ; MSP430 RESET Vector
-            .short  RESET                   ;
